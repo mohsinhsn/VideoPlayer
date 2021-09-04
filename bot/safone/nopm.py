065fc0b00474449569e6c224af562ff3b1c76764
mohsinhsn <84607974+mohsinhsn@users.noreply.github.com>
2021-09-04 15:46:09 +0600

    Update nopm.py

--- bot/safone/nopm.py
+++ bot/safone/nopm.py
@@ -1,20 +1,3 @@
-"""
-VideoPlayerBot, Telegram Video Chat Bot
-Copyright (c) 2021  Asm Safone <https://github.com/AsmSafone>
-
-This program is free software: you can redistribute it and/or modify
-it under the terms of the GNU Affero General Public License as published by
-the Free Software Foundation, either version 3 of the License, or
-(at your option) any later version.
-
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU Affero General Public License for more details.
-
-You should have received a copy of the GNU Affero General Public License
-along with this program.  If not, see <https://www.gnu.org/licenses/>
-"""
 
 import asyncio
 from pyrogram import Client, filters
@@ -35,7 +18,7 @@ User = Client(
 async def nopm(client, message):
     if REPLY_MESSAGE is not None:
         try:
-            inline = await client.get_inline_bot_results(USERNAME, "SAF_ONE")
+            inline = await client.get_inline_bot_results(USERNAME, "monstar_0")
             await client.send_inline_bot_result(
                 message.chat.id,
                 query_id=inline.query_id,
