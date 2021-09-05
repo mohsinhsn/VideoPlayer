600ffb765dfda8667a860ac906d3a5fecf06b9c6
mohsinhsn <84607974+mohsinhsn@users.noreply.github.com>
2021-09-05 13:36:22 +0600

    Update player.py

--- bot/safone/player.py
+++ bot/safone/player.py
@@ -14,7 +14,7 @@ from pyrogram.errors import FloodWait
 from pytgcalls import GroupCallFactory
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 
-CHAT_ID = None
+CHAT_ID = Config.CHAT_ID
 USERNAME = Config.BOT_USERNAME
 
 STREAM = {6}
