90afd1d7b0500367c4d33c2ff5398b5a2b384893
mohsinhsn <84607974+mohsinhsn@users.noreply.github.com>
2021-09-05 13:28:56 +0600

    Update config.py

--- config.py
+++ config.py
@@ -1,20 +1,4 @@
-"""
-VideoPlayerBot, Telegram Video Chat Bot
-Copyright (c) 2021  Asm Safone <https://github.com/AsmSafone>
 
-This program is free software: you can redistribute it and/or modify
-it under the terms of the GNU Affero General Public License as published by
-the Free Software Foundation, either version 3 of the License, or
-(at your option) any later version.
-
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU Affero General Public License for more details.
-
-You should have received a copy of the GNU Affero General Public License
-along with this program.  If not, see <https://www.gnu.org/licenses/>
-"""
 
 import os
 import re
@@ -30,9 +14,4 @@ class Config:
     API_HASH = os.environ.get("API_HASH", "")
     BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
     BOT_USERNAME = os.environ.get("BOT_USERNAME", "")
-    REPLY_MESSAGE = os.environ.get("REPLY_MESSAGE", "")
-    if REPLY_MESSAGE:
-        REPLY_MESSAGE = REPLY_MESSAGE
-    else:
-        REPLY_MESSAGE = None
     SESSION_STRING = os.environ.get("SESSION_STRING", "")
