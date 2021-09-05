8c77b7b8058c8de0f6bac6785a03c34a722f497a
mohsinhsn <84607974+mohsinhsn@users.noreply.github.com>
2021-09-05 12:39:14 +0600

    Update player.py

--- bot/safone/player.py
+++ bot/safone/player.py
@@ -1,69 +1,108 @@
-"""
-VideoPlayerBot, Telegram Video Chat Bot
-Copyright (c) 2021  Asm Safone <https://github.com/AsmSafone>
-
-This program is free software: you can redistribute it and/or modify
-it under the terms of the GNU Affero General Public License as published by
-the Free Software Foundation, either version 3 of the License, or
-(at your option) any later version.
-
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU Affero General Public License for more details.
-
-You should have received a copy of the GNU Affero General Public License
-along with this program.  If not, see <https://www.gnu.org/licenses/>
-"""
-
 import os
+import re
 import time
 import ffmpeg
 import asyncio
+from os import path
 from asyncio import sleep
 from config import Config
 from bot.safone.nopm import User
+from youtube_dl import YoutubeDL
 from pyrogram import Client, filters
 from pyrogram.types import Message
 from pyrogram.errors import FloodWait
 from pytgcalls import GroupCallFactory
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 
-ADMINS = Config.ADMINS
 CHAT_ID = Config.CHAT_ID
 USERNAME = Config.BOT_USERNAME
 
 STREAM = {6}
 VIDEO_CALL = {}
 
-# PyTgCalls Client
+ydl_opts = {
+        "format": "best",
+        "addmetadata": True,
+        "geo_bypass": True,
+        "nocheckcertificate": True,
+        "videoformat": "mp4",
+        "outtmpl": "downloads/%(id)s.%(ext)s",
+}
+ydl = YoutubeDL(ydl_opts)
 group_call_factory = GroupCallFactory(User, GroupCallFactory.MTPROTO_CLIENT_TYPE.PYROGRAM)
 
-@Client.on_message(filters.command(["stream", f"stream@{USERNAME}"]) & filters.user(ADMINS) & (filters.chat(CHAT_ID) | filters.private))
+@Client.on_message(filters.command(["stream", f"stream@{USERNAME}"]) & filters.group)
 async def stream(client, m: Message):
     if 1 in STREAM:
-        await m.reply_text("Please Stop The Existing Stream!")
+        await m.reply_text( Type /endstream to Stop The Existing Stream!**")
         return
+
     media = m.reply_to_message
-    if not media:
-        await m.reply_text("Reply To A Video To Stream!")
-        return
+    if not media and not ' ' in m.text:
+        await m.reply("❗ __Send Me A Live Stream Link / YouTube Video Link / Reply To A Video To Start Streaming!__")
+
+    elif ' ' in m.text:
+        msg = await m.reply_text("`Processing ...`")
+        text = m.text.split(' ', 1)
+        url = text[1]
+        regex = r"^(https?\:\/\/)?(www\.youtube\.com|youtu\.?be)\/.+"
+        match = re.match(regex,url)
+        if match:
+            await msg.edit(" `Starting YouTube Stream ...`")
+            try:
+                info = ydl.extract_info(url, False)
+                ydl.download([url])
+                ytvid = path.join("downloads", f"{info['id']}.{info['ext']}")
+            except Exception as e:
+                await msg.edit(f"YouTube Download Error! \n\n`{e}`")
+                return
+            await sleep(2)
+            try:
+                group_call = group_call_factory.get_group_call()
+                await group_call.join(CHAT_ID)
+                await group_call.start_video(ytvid)
+                VIDEO_CALL[CHAT_ID] = group_call
+                await msg.edit(f"▶Started [YouTube Streaming]({url})!")
+                try:
+                    STREAM.remove(0)
+                except:
+                    pass
+                try:
+                    STREAM.add(1)
+                except:
+                    pass
+            except Exception as e:
+                await msg.edit(f"unable to play please recheck and play again \n\nError: `{e}`")
+        else:
+            await msg.edit("`Starting Live Stream ...`")
+            live = url
+            await sleep(2)
+            try:
+                group_call = group_call_factory.get_group_call()
+                await group_call.join(CHAT_ID)
+                await group_call.start_video(live)
+                VIDEO_CALL[CHAT_ID] = group_call
+                await msg.edit(f"▶Started [Live Streaming]({live})!**")
+                try:
+                    STREAM.remove(0)
+                except:
+                    pass
+                try:
+                    STREAM.add(1)
+                except:
+                    pass
+            except Exception as e:
+                await msg.edit(f"unable to play please recheck and play again \n\nError: `{e}`")
+        else: \n\nError: `{e}`")
+
     elif media.video or media.document:
-        msg = await m.reply_text("Downloading, Please Wait...")
-        if os.path.exists(f'stream-{CHAT_ID}.raw'):
-            os.remove(f'stream-{CHAT_ID}.raw')
+        msg = await m.reply_text(" `Downloading ...`")
+        video = await client.download_media(media)
+        await sleep(2)
         try:
-            video = await client.download_media(media)
-            await msg.edit("processing, Please Wait...")
-            os.system(f'ffmpeg -i "{video}" -vn -f s16le -ac 2 -ar 48000 -acodec pcm_s16le -filter:a "atempo=0.81" stream-{CHAT_ID}.raw -y')
-        except Exception as e:
-            await msg.edit(f"something wrong, check it and try again \n`{e}`")
-            pass
-        await sleep(5)
-        group_call = group_call_factory.get_file_group_call(f'stream-{CHAT_ID}.raw')
-        try:
-            await group_call.start(CHAT_ID)
-            await group_call.set_video_capture(video)
+            group_call = group_call_factory.get_group_call()
+            await group_call.join(CHAT_ID)
+            await group_call.start_video(video)
             VIDEO_CALL[CHAT_ID] = group_call
             await msg.edit("▶Started Streaming!")
             try:
@@ -74,57 +113,18 @@ async def stream(client, m: Message):
                 STREAM.add(1)
             except:
                 pass
-        except FloodWait as e:
-            await sleep(e.x)
-            if not group_call.is_connected:
-                await group_call.start(CHAT_ID)
-                await group_call.set_video_capture(video)
-                VIDEO_CALL[CHAT_ID] = group_call
-                await msg.edit("▶Started Streaming!")
-                try:
-                    STREAM.remove(0)
-                except:
-                    pass
-                try:
-                    STREAM.add(1)
-                except:
-                    pass
         except Exception as e:
-            await msg.edit(f"something wrong, check it and try again \n`{e}`")
-            return
+            await msg.edit(f"unable to play please recheck and play again \n\nError: `{e}`")
+        else: \n\nError: `{e}`")
     else:
-        await m.reply_text("Reply To A Video To Stream!")
+        await m.reply_text("Send Me An Live Stream Link / YouTube Video Link / Reply To An Video To Start Streaming!__")
         return
 
 
-@Client.on_message(filters.command(["mute", f"mute@{USERNAME}"]) & filters.user(ADMINS) & (filters.chat(CHAT_ID) | filters.private))
-async def mute(_, m: Message):
-    if 0 in STREAM:
-        await m.reply_text("Please Start your video streaming!")
-        return
-    try:
-        await VIDEO_CALL[CHAT_ID].set_is_mute(True)
-        await m.reply_text("Muted Streamer!")
-    except Exception as e:
-        await m.reply_text(f"something wrong, check it and try again \n`{e}`")
-        return
-
-@Client.on_message(filters.command(["unmute", f"unmute@{USERNAME}"]) & filters.user(ADMINS) & (filters.chat(CHAT_ID) | filters.private))
-async def unmute(_, m: Message):
-    if 0 in STREAM:
-        await m.reply_text("Please Start The Stream First!")
-        return
-    try:
-        await VIDEO_CALL[CHAT_ID].set_is_mute(False)
-        await m.reply_text("🔉 **Unmuted Streamer!**")
-    except Exception as e:
-        await m.reply_text(f"Something wrong, check it and try again \n`{e}`")
-        return
-
-@Client.on_message(filters.command(["endstream", f"endstream@{USERNAME}"]) & filters.user(ADMINS) & (filters.chat(CHAT_ID) | filters.private))
+@Client.on_message(filters.command(["endstream", f"endstream@{USERNAME}"]) & filters.group)
 async def endstream(client, m: Message):
     if 0 in STREAM:
-        await m.reply_text("Please Start The Stream First!")
+        await m.reply_text("🤖 **Please Start The Stream First!**")
         return
     try:
         await VIDEO_CALL[CHAT_ID].stop()
@@ -138,13 +138,13 @@ async def endstream(client, m: Message):
         except:
             pass
     except Exception as e:
-        await m.reply_text(f"something wrong, check it and try again \n`{e}`")
-        return
+        await m.reply_text(f"unable to play please recheck and play again \n\nError: `{e}`")
+        else: \n\nError: `{e}`")
 
 
-admincmds=["stream", "pause", "resume", "endstream", f"stream@{USERNAME}", f"pause@{USERNAME}", f"resume@{USERNAME}", f"endstream@{USERNAME}"]
+admincmds=["stream", "endstream", f"stream@{USERNAME}", f"endstream@{USERNAME}"]
 
-@Client.on_message(filters.command(admincmds) & ~filters.user(ADMINS) & (filters.chat(CHAT_ID) | filters.private))
+@Client.on_message(filters.command(admincmds) & filters.group)
 async def notforu(_, m: Message):
     k = await m.reply_sticker("CAACAgUAAxkBAAEBpyZhF4R-ZbS5HUrOxI_MSQ10hQt65QACcAMAApOsoVSPUT5eqj5H0h4E")
     await sleep(5)
@@ -156,15 +156,15 @@ async def notforu(_, m: Message):
 
 allcmd = ["start", "help", f"start@{USERNAME}", f"help@{USERNAME}"] + admincmds
 
-@Client.on_message(filters.command(allcmd) & filters.group & ~filters.chat(CHAT_ID))
+@Client.on_message(filters.command(allcmd) & filters.group)
 async def not_chat(_, m: Message):
     buttons = [
             [
+                InlineKeyboardButton("CHANNEL", url="https://t.me/EnglishChatting_Club"),
                 InlineKeyboardButton("SUPPORT", url="https://t.me/StylishUser"),
             ],
             [
-                InlineKeyboardButton("owner", url="https://t.me/monstar_0"),
+                InlineKeyboardButton("OWNER", url="https://t.me/monstar_0"),
             ]
          ]
-    await m.reply_text(text="**Sorry, You Can't Use This Bot In This Group!**", reply_markup=InlineKeyboardMarkup(buttons), disable_web_page_preview=True)
-
+    await m.reply_text(text="Sorry, You Can't Use This Bot In This Group ")
